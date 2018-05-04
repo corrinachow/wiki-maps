54fb319b2c15ae3a7070219b503c739e479488b3
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-04 16:59:08 +0000

    Users/:id displays user info, maps created by user, and contributions to which maps

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,66 @@
 const express = require("express");
 const router = express.Router();
 
+function getMapMarkers(data) {
+  const result = [];
+  for (const obj of data) {
+    result.push({
+      marker_id: obj.marker_id,
+      marker_title: obj.marker_title,
+      marker_description: obj.marker_description,
+      marker_coordinates: obj.marker_coordinates,
+      marker_img_url: obj.marker_img_url,
+      marker_created_by: obj.marker_created_by
+    });
+  }
+  return result;
+};
+
+function aggregateData(data) {
+  const userData = {};
+
+  for (const dataItem of data) {
+    userData[dataItem.user_id] = userData[dataItem.user_id] || {
+      user_id: dataItem.user_id,
+      email: dataItem.email,
+      username: dataItem.username
+    };
+
+    userData.maps = userData.maps || [];
+    userData.contributions = userData.contributions || [];
+
+    const filteredData = data.filter(
+      dataObj => dataObj.map_id === dataItem.map_id
+    );
+    const mapObj = {
+      map_id: filteredData[0].map_id,
+      map_title: filteredData[0].map_title,
+      map_coordinates: filteredData[0].map_coordinates
+    };
+    const filterMap = userData.maps.filter(
+      fMapObj => fMapObj.map_id === mapObj.map_id
+    );
+    const contrObj = {
+      marker_id: dataItem.marker_id,
+      map_id: dataItem.map_id,
+      marker_title: dataItem.marker_title,
+      marker_description: dataItem.marker_description,
+      marker_coordinates: dataItem.marker_coordinates,
+      marker_img_url: dataItem.marker_img_url
+    };
+    const filterContr = userData.contributions.filter(
+      fContrObj => fContrObj.marker_id === contrObj.marker_id
+      );
+
+    if (filterMap.length < 1) {
+      userData.maps.push(mapObj);
+    } else if (filterContr.length < 1) {
+      userData.contributions.push(contrObj);
+    }
+  }
+  return userData;
+}
+
 module.exports = knex => {
   router.get("/", (req, res) => {
     knex("users")
@@ -15,15 +75,12 @@ module.exports = knex => {
   }),
     router.get("/:id", (req, res) => {
       knex("users")
-        .join("contributions", "users.id", "contributions.user_id")
-        .join("maps", "maps.id", "contributions.map_id")
-        .join("markers", "maps.id", "markers.map_id")
+        .join("maps", "users.id", "maps.user_id")
+        .join("markers", "users.id", "markers.user_id")
         .select(
           "users.id as user_id",
           "email",
           "username",
-          "contributions.id as contribution_id",
-          "contributions.action",
           "maps.id as map_id",
           "maps.title as map_title",
           "maps.coordinates as map_coordinates",
@@ -32,11 +89,13 @@ module.exports = knex => {
           "markers.description as marker_description",
           "markers.image_url as marker_img_url",
           "markers.coordinates as marker_coordinates",
-          "markers.active"
+          "markers.map_id as map_id"
         )
         .where({ "users.id": req.params.id })
         .then(user => {
-          res.json(user);
+          console.log(user)
+          const parseData = aggregateData(user);
+          res.json(parseData);
         });
     }),
     router.get("/:id/favourites", (req, res) => {
@@ -52,10 +111,3 @@ module.exports = knex => {
 
   return router;
 };
-
-// user_contributions: [{contribution_id:info},
-//                       {contribution2:info}
-//                       ]
-
-//                     action:
-//                     map_id:
